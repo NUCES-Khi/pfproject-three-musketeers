b3a9956408875031a14330470143ccd35902b391
ukkashah11 <115150510+ukkashah11@users.noreply.github.com>
2023-11-30 00:01:40 +0500

    Update project_connect4.c

--- project_connect4.c
+++ project_connect4.c
@@ -43,6 +43,17 @@ int get_first_empty(char **grid, int col);
 // set the grid at the specified column and row with the player's character
 void set_grid(char **grid, int col, int player);
 
+// check the win status of the game after a move
+bool game_win_status(char **grid, int col, int player);
+
+// check if the game is a draw
+bool game_draw_status(char **grid);
+
+// shows the wins for each player
+void display_wins(int player);
+
+// extracts player wins from the wins file
+int *save_win(int player);
 
 int main()
 {
@@ -211,3 +222,116 @@ void set_grid(char **grid, int col, int player)
 	int row = get_first_empty(grid, col);
 	grid[row][col] = (player) ? P1 : P2;
 } // end set_grid()
+
+bool game_win_status(char **grid, int col, int player)
+{
+	// getting row of current player character
+	char player_char = (player) ? P1 : P2;
+	int row = get_first_empty(grid, col)+1;
+
+	// down
+	if (row + 3 < rows) {
+		if (grid[row+1][col] == player_char && grid[row+2][col] == player_char && grid[row+3][col] == player_char)
+				return true;
+	}
+
+	// right
+	if (col + 3 < cols) {
+		if (grid[row][col+1] == player_char && grid[row][col+2] == player_char && grid[row][col+3] == player_char)
+				return true;
+	}
+
+	// left
+	if (col - 3 >= 0) {
+		if (grid[row][col-1] == player_char && grid[row][col-2] == player_char && grid[row][col-3] == player_char)
+				return true;
+	}
+
+	// up right
+	if (col + 3 < cols && row - 3 >= 0) {
+		if (grid[row-1][col+1] == player_char && grid[row-2][col+2] == player_char && grid[row-3][col+3] == player_char)
+				return true;
+	}
+
+
+	// up left
+	if (col - 3 >= 0 && row - 3 >= 0) {
+		if (grid[row-1][col-1] == player_char && grid[row-2][col-2] == player_char && grid[row-3][col-3] == player_char)
+				return true;
+	}
+
+	// down right
+	if (col + 3 < cols && row + 3 < rows) {
+		if (grid[row+1][col+1] == player_char && grid[row+2][col+2] == player_char && grid[row+3][col+3] == player_char)
+				return true;
+	}
+
+	// down left
+	if (col - 3 >= 0 && row + 3 < rows) {
+		if (grid[row+1][col-1] == player_char && grid[row+2][col-2] == player_char && grid[row+3][col-3] == player_char)
+				return true;
+	}
+
+	return false;
+} // end game_win_status()
+
+bool game_draw_status(char **grid)
+{
+	// if top row is not full return false
+	for (int i = 0; i < cols; i++) if (grid[0][i] == X) return false;
+	return true;
+} // end game_draw_status()
+
+
+void display_wins(int player)
+{
+	// get wins from the file
+	int *wins = save_win(player);
+	printf("\nTotal %c wins: %d\nTotal %c wins: %d", P1, wins[1], P2, wins[0]);
+
+	// to not immediately close the program at the end
+	Sleep(10000);
+} // end display_wins()
+
+int *save_win(int player)
+{
+	char line[BUFFER_SIZE];
+	int *wins = (int *)calloc(2, sizeof(int));
+
+	//reading wins from file
+	FILE *f_ptr = fopen(f_name, "r+");
+
+	// if file exists
+	if (f_ptr != NULL) {
+		for (int i = 0;!feof(f_ptr); i++) {
+			// reading win from file
+			fgets(line, BUFFER_SIZE, f_ptr);
+
+			// trimming string
+			line[strcspn(line, "\n")] = 0;
+
+			// adding into win of the player
+			// ignoring if -1 (DRAW)
+			wins[i] = atoi(line) + (player != -1 && i == player);
+		}
+
+		fclose(f_ptr);
+	} else wins[player] = 1; // setting to 1 if no file exists
+
+
+	// ending if DRAW
+	if (player == -1) return wins;
+
+
+	// writing new wins in file
+	f_ptr = fopen(f_name, "w");
+
+	// if file can be opened
+	if (f_ptr != NULL) {
+		// writing the wins in to the file
+		fprintf(f_ptr, "%d\n%d", wins[0], wins[1]);
+		fclose(f_ptr);
+	}
+
+	return wins;
+} // end save_win()
