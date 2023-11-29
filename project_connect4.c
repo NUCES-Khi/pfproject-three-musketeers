08ade815f8a3d678e3ecbfb112f2db7099d9db59
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-11-29 19:26:05 +0500

    Create project_connect4.c

---
+++ project_connect4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+
+#define BUFFER_SIZE 10
+#define f_name "wins.txt"
+
+#define P1 'R'
+#define P2 'B'
+#define X '.'
+
+// default settings
+const int rows = 6, cols = 7;
